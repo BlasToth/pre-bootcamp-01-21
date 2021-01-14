8d557f4e5735129f4259e8fe24ea6fa9aae1e255
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-14 10:30:06 +0100

    add loop for numbers

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,34 @@
 const display = document.querySelector('#display');
 display.textContent = "0";
 const numbers = document.querySelectorAll('.numbers');
+const clear = document.querySelector('#clear');
 
-numbers[0].addEventListener('click',() => {
-    display.textContent += numbers[0].textContent;
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click',() => {
+        if (display.textContent === "0"){
+            display.textContent = numbers[i].textContent;
+        } else {
+            display.textContent += numbers[i].textContent;
+        }
+    });
+}
+
+
+// numbers[1].addEventListener('click',() => {
+//     if (display.textContent === "0"){
+//         display.textContent = numbers[1].textContent;
+//     } else {
+//         display.textContent += numbers[1].textContent;
+//     }
+// });
+// numbers[2].addEventListener('click',() => {
+//     if (display.textContent === "0"){
+//         display.textContent = numbers[2].textContent;
+//     } else {
+//         display.textContent += numbers[2].textContent;
+//     }
+// });
+
+clear.addEventListener('click',() => {
+    display.textContent = "0";
 });
-numbers[1].addEventListener('click',() => {
-    display.textContent += numbers[1].textContent;
-});
-
-
-console.log(numbers[2].textContent);
