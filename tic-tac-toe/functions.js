9c38fa0de35ca44c6f8677bf79b8acff60694901
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-20 14:57:37 +0100

    refactor code

---
+++ tic-tac-toe/functions.js
@@ -0,0 +1,144 @@
+function win() {
+    // horizontal check
+    if ( board[0] === player1 && board[1] === player1 && board[2] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[0] === player2 && board[1] === player2 && board[2] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[3] === player1 && board[4] === player1 && board[5] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[3] === player2 && board[4] === player2 && board[5] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[6] === player1 && board[7] === player1 && board[8] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[6] === player2 && board[7] === player2 && board[8] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    // vertical check
+    if ( board[0] === player1 && board[3] === player1 && board[6] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[0] === player2 && board[3] === player2 && board[6] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[1] === player1 && board[4] === player1 && board[7] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[1] === player2 && board[4] === player2 && board[7] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[2] === player1 && board[5] === player1 && board[8] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[2] === player2 && board[5] === player2 && board[8] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    // diagonal check
+    if ( board[0] === player1 && board[4] === player1 && board[8] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[0] === player2 && board[4] === player2 && board[8] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[2] === player1 && board[4] === player1 && board[6] === player1 ) {
+      winner = true;
+      alert("WINNER PLAYER1 - X");
+          setTimeout(()=> {
+        endGame();
+      }, 2000); 
+  
+    }
+    if ( board[2] === player2 && board[4] === player2 && board[6] === player2 ) {
+      winner = true;
+      alert("WINNER PLAYER2 - O");
+          setTimeout(()=> {
+        endGame();
+      }, 2000);   
+  }
+}
+
+function endGame() {
+  
+    if (winner === true) {
+      for (let i = 0; i < 9; i++) {
+        grid[i].innerText = "";
+        grid[i].style.backgroundColor = "rgb(218, 184, 140)";
+        board[i] = "";
+        winner = false;
+      }
+    }
+  }
