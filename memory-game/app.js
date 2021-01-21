f617e3074888ca24d118238cae9265a460ae7e5d
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-21 20:07:54 +0100

    add shuffle function

--- memory-game/app.js
+++ memory-game/app.js
@@ -2,12 +2,30 @@ const images = document.querySelectorAll('.cell');
 const background = ["apple", "banana", "fig", "lemon", "mango", "pear", "apple", "banana", "fig", "lemon", "mango", "pear"];
 
 
-
+const randomBg = randomArrayShuffle(background);
 
 for (let i = 0; i < images.length; i++) {
     
     images[i].addEventListener("click", ()=> {
-        images[i].classList.toggle("turned");
-            images[i].style.backgroundImage = `url(images/${background[i]}.png)`;
+        images[i].style.backgroundImage = `url(images/${background[i]}.png)`;
+        setTimeout(()=> {
+            images[i].style.backgroundImage = "url(images/blank.png)";
+        }, 1500);
+        // images[i].classList.toggle("turned");
     })
 }
+
+
+function randomArrayShuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  
